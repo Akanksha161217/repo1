4b3bee64e187d3ba093a2e71db1f5509247c56d8
unknown <abc@.com>
2024-06-25 12:14:38 +0530

    commit done..

---
+++ treemap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Test{
+	public static void main(String args[]){
+		TreeMap Tmap=new TreeMap();
+		Tmap.put(6,7);
+		Tmap.put(3,"Hello");
+		Tmap.put(6,18);
+		Tmap.put(7,34.89);
+		System.out.println(Tmap);
+
+	}
+}
