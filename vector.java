8155a70a554eb0d188bc266cee269be0575f8f17
unknown <abc@.com>
2024-06-25 11:26:38 +0530

    first commit done..

---
+++ vector.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Test
+{
+	public static void main(String args[])
+	{
+	Vector vt=new Vector();
+	vt.add(87);
+	vt.add(88);
+	vt.add(87.46);
+	vt.add(true);
+	vt.add("False");
+	System.out.println(vt);
+	vt.add(87.878);
+	vt.add(75687);
+	vt.add(8.4);	
+	vt.add("sgm");
+	System.out.println(vt);
+	}
+}
